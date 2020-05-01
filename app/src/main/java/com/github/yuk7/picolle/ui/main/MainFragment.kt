f300d8a89eeaa8fff2552d5cc2366efc2b26af66
yuk7 <yukx00@gmail.com>
2020-05-02 03:31:53 +0900

    ui.main: initial databinding

--- app/src/main/java/com/github/yuk7/picolle/ui/main/MainFragment.kt
+++ app/src/main/java/com/github/yuk7/picolle/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
 package com.github.yuk7.picolle.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.github.yuk7.picolle.R
+import com.github.yuk7.picolle.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
@@ -14,17 +15,14 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val mainViewModel = MainViewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater, R.layout.fragment_main, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = mainViewModel
+        return binding.root
     }
 
 }
--- app/src/main/java/com/github/yuk7/picolle/ui/main/MainViewModel.kt
+++ app/src/main/java/com/github/yuk7/picolle/ui/main/MainViewModel.kt
@@ -3,5 +3,6 @@ package com.github.yuk7.picolle.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var searchWord = ""
+
 }
